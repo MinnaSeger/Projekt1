ab9a5022f0d5c2beba2a6d9582f5cbef9f46d8b3
MinnaSeger <minna.seger@hotmail.com>
2024-12-19 20:24:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projekt1
+ Systemutvecklingsprojekt 24
---
+++ src/projekt1/Projekt1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projekt1;
+
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author minna
+ */
+public class Projekt1 {
+    
+    private static InfDB idb;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try{
+            idb = new InfDB("ngo_2024", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            new Inloggning(idb).setVisible(true); 
+            System.out.println("funkar");
+        }
+    catch(InfException ex){
+        System.out.println(ex.getMessage());
+        }    
+    }
+    
+}
